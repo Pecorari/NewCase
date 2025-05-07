f5c3dba15dc5bcbac65d322e8eca445d133bb9a7
Pecorari <pecorari.clemente@gmail.com>
2025-05-07 20:05:49 -0300

    adicionando variavel de ambiente no react

--- frontend/src/hooks/useApi.js
+++ frontend/src/hooks/useApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.10:3333/',
+  baseURL:  process.env.REACT_APP_API_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
