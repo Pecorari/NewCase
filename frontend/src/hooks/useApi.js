2e6740597709e1b8b3f9412e1b4d1e29486c309f
Pecorari <pecorari.clemente@gmail.com>
2025-05-07 11:55:05 -0300

    baseURL API

--- frontend/src/hooks/useApi.js
+++ frontend/src/hooks/useApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://192.168.1.102:3333',
+  baseURL: 'http://192.168.0.10:3333/',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
