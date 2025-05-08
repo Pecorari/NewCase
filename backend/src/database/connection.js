e4637498b121fe862de4881416fd62e0946d6458
Pecorari <pecorari.clemente@gmail.com>
2025-05-08 11:18:48 -0300

    arrumando connection

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,18 +1,23 @@
 const mysql = require('mysql2/promise');
-
 require('dotenv').config();
 
-const connection = mysql.createPool({
+const connection = async config => {
+    return mysql.connection({
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWD,
         database: process.env.MYSQL_NAME,
-        socketPath: process.env.INSTANCE_UNIX_SOCKET
-});
+        host: process.env.MYSQL_HOST,
+        port: 3306,
+        ...config,
+    });
+};
 
 async function testConnection() {
     try {
-        await connection.getConnection();
+        const pool = await connection();
+        const conn = await pool.getConnection();
         console.log('Conexão com o MySQL estabelecida com sucesso!');
+        conn.release();
     } catch (error) {
         console.error('Erro ao conectar com o MySQL:', error.message);
     }
