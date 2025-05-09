9e404151c1bd98cb64d52e8721b5cbc2a3a31de2
Pecorari <pecorari.clemente@gmail.com>
2025-05-09 18:15:40 -0300

    Arrumando o caminho do useApi

--- frontend/src/services/freteService.js
+++ frontend/src/services/freteService.js
@@ -1,4 +1,4 @@
-import api from '../../hooks/useApi';
+import api from '../hooks/useApi';
 
 export async function calcularFrete(cepDestino, produto) {
   const response =await api.post('https://mycell-store-700054947412.us-west1.run.app/calcular-frete',
