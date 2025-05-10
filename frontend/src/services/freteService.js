2b9173ed39307b001e65ad222be2ed3dec610aca
Pecorari <pecorari.clemente@gmail.com>
2025-05-10 10:20:55 -0300

    Arrumando requisição para rota /calcular-frete

--- frontend/src/services/freteService.js
+++ frontend/src/services/freteService.js
@@ -1,7 +1,7 @@
 import api from '../hooks/useApi';
 
 export async function calcularFrete(cepDestino, produto) {
-  const response =await api.post('https://mycell-store-700054947412.us-west1.run.app/calcular-frete',
+  const response =await api.post('/calcular-frete',
     { cep_destino: cepDestino,
       peso: produto.peso,
       comprimento: produto.comprimento,
