02b5a754c974904ce851584f2559911b41ff650d
Pecorari <pecorari.clemente@gmail.com>
2025-05-18 15:38:59 -0300

    Adicionando condição para disparar o carrinhoQtd

--- frontend/src/context/CarrinhoContext.js
+++ frontend/src/context/CarrinhoContext.js
@@ -1,17 +1,23 @@
 import { createContext, useContext, useState, useEffect } from 'react';
+import { useAuth } from "./AuthContext";
 import api from '../hooks/useApi';
 
 const CarrinhoContext = createContext();
 
 export const CarrinhoProvider = ({ children }) => {
   const [qtdCarrinho, setQtdCarrinho] = useState(0);
+  const { usuario } = useAuth();
 
   const atualizarQtdCarrinho = async () => {
-    try {
-      const response = await api.get('/carrinhoQtd');
-      setQtdCarrinho(response.data);
-    } catch (err) {
-      console.error('Erro ao atualizar carrinho:', err);
+    if(usuario) {
+      try {
+        const response = await api.get('/carrinhoQtd');
+        setQtdCarrinho(response.data);
+      } catch (err) {
+        console.error('Erro ao atualizar carrinho:', err);
+      }
+    } else {
+      setQtdCarrinho(0);
     }
   };
 
