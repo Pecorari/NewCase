0eb5bb09add071727e8428764be09e574cb94d6b
Pecorari <pecorari.clemente@gmail.com>
2025-09-20 21:09:32 -0300

    teste3

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
 });
